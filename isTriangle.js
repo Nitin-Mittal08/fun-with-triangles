07b5acb06c97ba88a4c05342fc07740588143ac8
Nitin Mittal <nitin2000mittal@gmail.com>
2022-12-03 14:16:34 +0530

    isTriangle done

---
+++ isTriangle.js
@@ -0,0 +1,24 @@
+const inputs = document.querySelectorAll(".input-angle");
+const checkBtn = document.querySelector("#check-btn");
+const output = document.querySelector("#output");
+
+function sumOfAnglesFunction(a,b,c){
+    const sumAngle = a + b +c;
+    return sumAngle;
+}
+
+
+function checkIfTriangle(){
+    
+    const sumOfAngles = sumOfAnglesFunction(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
+    if(sumOfAngles==180){
+        output.innerText = "Yay! It is a Triangle.🙌"
+    }else{
+        output.innerText = "Oh no, It isn't a Triangle.🤷‍♀️"
+    }
+    //console.log("Clicked");
+}
+
+
+
+checkBtn.addEventListener("click", checkIfTriangle);
