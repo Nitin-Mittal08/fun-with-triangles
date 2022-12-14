ee8570e55ed2a4e1d03086388b6fdfaca1fddf69
Nitin Mittal <72539882+Nitin-Mittal08@users.noreply.github.com>
2022-12-14 12:50:44 +0530

    Update area.js

--- area.js
+++ area.js
@@ -4,14 +4,18 @@ const outputEl = document.querySelector("#output");
 
 function triangleArea(a,b,c){
     const s = (a+b+c)/2
-    const area = Math.sqrt([s * (s-a) * (s-b) * (s-c)]);
+    const area = Math.sqrt([s * (s-a) * (s-b) * (s-c)]).toFixed(2);
     return area;
 }
 
 function calculateArea(){
+    if(Number(inputs[0].value)>0 && Number(inputs[1].value)>0 && Number(inputs[2].value)>0){
     const triangleA = triangleArea(Number(inputs[0].value),Number(inputs[1].value), Number(inputs[2].value));
 
     outputEl.innerText = "The area of the Triangle is: "  + triangleA;
+    }else{
+        outputEl.innerText = "Enter positive values for sides of triangle";
+    }
 }
 
 
