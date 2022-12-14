76728dc7033636b6f7ce5f193d8a3c9fa72b6a29
Nitin Mittal <72539882+Nitin-Mittal08@users.noreply.github.com>
2022-12-14 12:40:02 +0530

    Update isTriangle.js

--- isTriangle.js
+++ isTriangle.js
@@ -10,12 +10,19 @@ function sumOfAnglesFunction(a,b,c){
 
 function checkIfTriangle(){
     
-    const sumOfAngles = sumOfAnglesFunction(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
+    if(Number(inputs[0].value)>0 && Number(inputs[1].value)>0 && Number(inputs[2].value)>0){
+       const sumOfAngles = sumOfAnglesFunction(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
     if(sumOfAngles==180){
         output.innerText = "Yay! It is a Triangle.🙌"
     }else{
         output.innerText = "Oh no, It isn't a Triangle.🤷‍♀️"
-    }
+    } 
+  }else{
+      output.innerText = "Enter positive values for angles of triangle";
+  }
+    
+    
+    
 }
 
 
